3c79213749f592b054cbdc1e0d9dce2d9d238d5f
Madman10K <stanv.contact176@gmail.com>
2023-08-31 20:21:41 +0300

    Fix CI

--- ci.sh
+++ ci.sh
@@ -3,5 +3,5 @@ cpus=$(grep -c processor /proc/cpuinfo) || cpus=$(sysctl -n hw.ncpu)
 
 mkdir build
 cd build || exit
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE || exit
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_VARIANT_VENDOR=ON || exit
 MSBuild.exe UntitledI18N.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit

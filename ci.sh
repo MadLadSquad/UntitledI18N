be8cf0b0ad02ddc0aaf27892cb89bb64c528e312
Madman10K <stanv.contact176@gmail.com>
2026-06-09 04:37:41 +0300

    Adapt for new VS versions

--- ci.sh
+++ ci.sh
@@ -4,4 +4,4 @@ cpus=$(grep -c processor /proc/cpuinfo) || cpus=$(sysctl -n hw.ncpu)
 mkdir build
 cd build || exit
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_VARIANT_VENDOR=ON || exit
-MSBuild.exe UntitledI18N.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
+MSBuild.exe UntitledI18N.sln* -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
